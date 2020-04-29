7fffc14e5d26b4d1700efb38b785480b6285a94c
Erin Mettille <ecmettille@gmail.com>
2020-04-29 15:29:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize (name)
+    @name = name 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end 
+  
+  def add_song (song)
+    song.artist = self 
+  end
+  
+  def self.find_or_create_by_name (name)
+    @@all.find {|artist| artist == name}
+    
+    song = Song.new(title)
+    add_song(song)
+  end
+    
+end
