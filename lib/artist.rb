247638e3630066bb9d2974001bf760b2abca5bfd
Erin Mettille <ecmettille@gmail.com>
2020-04-29 15:43:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
   
   def self.find_or_create_by_name (name)
     artist = @@all.find {|artist| artist == name}
-    if artist == nil Artist.new (name)
+    if artist == nil { Artist.new (name) }
   end
     
 end
